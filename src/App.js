db33f0f81b3c0b0e7a03359be88c2e75638a05f3
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-07-17 01:40:17 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Markdown from 'markdown-to-jsx';
+import AceEditor from 'react-ace';
+import style from 'styled-components';
+import brace from 'brace';
+import 'brace/mode/markdown';
+import 'brace/theme/dracula';
+import './App.css';
+
+const { ipcRenderer } = window.require('electron');
+
+class App extends Component {
+  state = {
+    loadedFile: ''
+  }
+  constructor() {
+    super();
+
+    ipcRenderer.on('new-file', (event, fileContent) => {
+      this.setState({
+        loadedFile: fileContent
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Split>
+          <CodeWindow>
+            <AceEditor
+              mode="markdown"
+              theme="dracula"
+              onChange={newContent => {
+                this.setState({
+                  loadedFile: newContent
+                });
+              }}
+              name="markdown_editor"
+              value={this.state.loadedFile}
+            />
+          </CodeWindow>
+          <RenderedWindow>
+            <Markdown>{this.state.loadedFile}</Markdown>
+          </RenderedWindow>
+      </Split>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+const Split = style.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const CodeWindow = style.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`;
+
+const RenderedWindow = style.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #82d8d8;
+  }
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #e54b4b;
+  }
+`;
